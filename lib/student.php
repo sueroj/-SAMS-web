de9fc04ec29423d8401a7c52ef3a8456274be399
J <sueroj@outlook.com>
2020-02-05 02:41:30 +0000

    Added db functions & worked on class Student

---
+++ lib/student.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types=1);
+include "db_functions.php";
+
+class Student
+{
+    private $id;
+
+    private $firstname;
+
+    private $lastname;
+
+    private $course;
+
+    private $module;
+
+    private $attended;
+
+    private $password;
+
+    public function __construct(int $_id, string $_first, string $_last, string $_course)
+    {
+        $this->id = $_id;
+        $this->firstname = $_first;
+        $this->lastname = $_last;
+        $this->course = $_course;
+    }
+
+    public function addStudent()
+    {
+       createTable($this->id, $this->firstname, $this->lastname, $this->course);
+    }
+
+    //Data input security check
+    private function checkData(string $data)
+    {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+    }
+}
+?>
